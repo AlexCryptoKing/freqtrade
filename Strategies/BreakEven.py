0411df5fc9e7f820e247ceeaeadd8791cebcdb18
AlexCryptoKing <108023339+AlexCryptoKing@users.noreply.github.com>
2024-10-12 12:48:27 +0200

    Add files via upload

--- Strategies/BreakEven.py
+++ Strategies/BreakEven.py
@@ -1,7 +1,6 @@
 # --- Do not remove these libs ---
-from freqtrade.strategy import IStrategy
+from freqtrade.strategy.interface import IStrategy
 from pandas import DataFrame
-
 # --------------------------------
 
 
@@ -13,13 +12,13 @@ class BreakEven(IStrategy):
 
     I can "/stopbuy" and wait for the positions to get closed by the bot rules, which is
     waiting for some profit, etc -- this usually takes too long...
-
+    
     What I would prefer is to close everything that is over 0% profit to avoid the losses.
 
     Here's a simple strategy with empty buy/sell signals and "minimal_roi = { 0 : 0 }" that
     sells everything already at profit and wait until the positions at loss will come to break
     even point (or the small profit you provide in ROI table).
-
+    
     You may restart the bot with the new strategy as a command-line parameter.
 
     Another way would be to specify the original strategy in the config file, then change to
@@ -27,11 +26,10 @@ class BreakEven(IStrategy):
 
     """
 
-    INTERFACE_VERSION: int = 3
     # This attribute will be overridden if the config file contains "minimal_roi"
     minimal_roi = {
         "0": 0.01,      # at least 1% at first
-        "10": 0         # after 10min, everything goes
+        "10": 0         # after 10min, everything goes 
     }
 
     # This is more radical version that sells everything above the profit level
@@ -54,16 +52,16 @@ class BreakEven(IStrategy):
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         return dataframe
 
-    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
             ),
-            'enter_long'] = 0
+            'buy'] = 0
         return dataframe
 
-    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
             ),
-            'exit_long'] = 0
+            'sell'] = 0
         return dataframe
